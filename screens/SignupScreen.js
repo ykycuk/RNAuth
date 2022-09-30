31cee00a593686024f268d78cc9825cdaec38c39
BatyrArtem <artembatyr88@gmail.com>
2022-09-30 10:53:18 +0300

    auth add

---
+++ screens/SignupScreen.js
@@ -0,0 +1,34 @@
+import {useContext, useState} from "react";
+
+import AuthContent from '../components/Auth/AuthContent';
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
+import {Alert} from "react-native";
+
+function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false); // to show loading screen
+
+  const authCtx = useContext(AuthContext);
+
+  async function signupHandler({email, password}) {
+    setIsAuthenticating(true);
+    try {
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert('Authentication failed',
+          'Could not create user, please check your input and try again later!'
+      );
+      setIsAuthenticating(false);
+    }
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..."/>
+  }
+
+  return <AuthContent onAuthenticate={signupHandler}/>;
+}
+
+export default SignupScreen;
---
+++ util/auth.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const API_KEY = "AIzaSyA4MwJaMQbFFVMOOLIwZof1H-xvSmtamVY";
+
+async function authenticate(mode, email, password) {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+    const response = await axios.post(
+        url,
+        {
+            email,
+            password,
+            returnSecureToken: true,
+        }
+    );
+
+    const token = response.data.idToken;
+
+    return token;
+}
+
+export function createUser(email, password) {
+    return authenticate('signUp', email, password);
+}
+
+export function login(email, password) {
+    return authenticate('signInWithPassword', email, password);
+}
